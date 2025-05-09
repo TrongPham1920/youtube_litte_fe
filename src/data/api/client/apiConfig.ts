51f076e5a9928500b5e5aa1e809474d3bdd8a802
TRONG <phamtrong1920@gmail.com>
2025-05-10 02:09:24 +0700

    no message

--- src/data/api/client/apiConfig.ts
+++ src/data/api/client/apiConfig.ts
@@ -1,20 +1,20 @@
 // apiConfig.ts
 
 export const API_PATH = {
-    LOGIN: getPath("login"),
-    REGISTER: getPath("register"),
-    VIDEOS: getPath("videos"),
-    VIDEO_BY_ID: (id: string) => getPath(`videos/${id}`),
+  LOGIN: getPath("login"),
+  REGISTER: getPath("register"),
+  VIDEOS: getPath("videos"),
+  VIDEO_BY_ID: (id: string) => getPath(`videos/${id}`),
 
-    CHANNELS: getPath("channels"),
-    CHANNEL_BY_ID: (id: string) => getPath(`channels/${id}`),
-    CHANNEL_BY_USER_ID: (userId: string) => getPath(`channels/user/${userId}`),
-    VIDEOS_BY_CHANNEL: (channelId: string) => `videos/channel/${channelId}`,
-    UPLOAD_IMAGE: getPath("upload-image"),
+  CHANNELS: getPath("channels"),
+  CHANNEL_BY_ID: (id: string) => getPath(`channels/${id}`),
+  CHANNEL_BY_USER_ID: (userId: string) => getPath(`channels/user/${userId}`),
+  VIDEOS_BY_CHANNEL: (channelId: string) => `videos/channel/${channelId}`,
+  UPLOAD_IMAGE: getPath("upload-image"),
 };
 
 function getPath(path: string): string {
-    return `http://localhost:5000/api/v1/${path}`;
+  return `http://14.225.212.252:5000/api/v1/${path}`;
 }
 
 export default API_PATH;
